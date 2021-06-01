27133dac0c82bf4cdefa66406b99b4d3ba01b8f9
takwa <blgeyatatou@gmail.com>
2021-06-01 11:35:54 +0100

    front

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,2 +1,15 @@
 import { combineReducers } from "redux";
-export default combineReducers({});
+import auth from "./auth.reducers";
+import menu from "./menu.reducers";
+import restaurant from "./restaurant.reducers";
+import order from "./order.reducers";
+import product from "./product.reducers";
+import category from "./category.reducers";
+export default combineReducers({
+  auth,
+  menu,
+  restaurant,
+  category,
+  product,
+  order,
+});
